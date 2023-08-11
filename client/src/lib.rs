d3bec4ecfee0707cc12a914a0b968d4bee3a73b6
TheSunSide <60361981+TheSunSide@users.noreply.github.com>
2023-08-11 05:54:26 -0400

    RISE

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -44,27 +44,27 @@ pub(crate) fn ChessBoardComponent(cx: Scope<()>, board: UseRef<ChessBoard>) -> E
 
 fn populate_board<'a>(board: &'a mut Board) {
     board[0][0] = Some((PieceKind::Rook, Color::White));
-    board[0][1] = Some((PieceKind::Knight, Color::White));
-    board[0][2] = Some((PieceKind::Bishop, Color::White));
-    board[0][3] = Some((PieceKind::Queen, Color::White));
-    board[0][4] = Some((PieceKind::King, Color::White));
-    board[0][5] = Some((PieceKind::Bishop, Color::White));
-    board[0][6] = Some((PieceKind::Knight, Color::White));
-    board[0][7] = Some((PieceKind::Rook, Color::White));
+    board[1][0] = Some((PieceKind::Knight, Color::White));
+    board[2][0] = Some((PieceKind::Bishop, Color::White));
+    board[3][0] = Some((PieceKind::Queen, Color::White));
+    board[4][0] = Some((PieceKind::King, Color::White));
+    board[5][0] = Some((PieceKind::Bishop, Color::White));
+    board[6][0] = Some((PieceKind::Knight, Color::White));
+    board[7][0] = Some((PieceKind::Rook, Color::White));
     for n in 0..8 {
-        board[1][n] = Some((PieceKind::Pawn, Color::White));
+        board[n][1] = Some((PieceKind::Pawn, Color::White));
     }
 
-    board[7][0] = Some((PieceKind::Rook, Color::Black));
-    board[7][1] = Some((PieceKind::Knight, Color::Black));
-    board[7][2] = Some((PieceKind::Bishop, Color::Black));
-    board[7][3] = Some((PieceKind::Queen, Color::Black));
-    board[7][4] = Some((PieceKind::King, Color::Black));
-    board[7][5] = Some((PieceKind::Bishop, Color::Black));
-    board[7][6] = Some((PieceKind::Knight, Color::Black));
+    board[0][7] = Some((PieceKind::Rook, Color::Black));
+    board[1][7] = Some((PieceKind::Knight, Color::Black));
+    board[2][7] = Some((PieceKind::Bishop, Color::Black));
+    board[3][7] = Some((PieceKind::Queen, Color::Black));
+    board[4][7] = Some((PieceKind::King, Color::Black));
+    board[5][7] = Some((PieceKind::Bishop, Color::Black));
+    board[6][7] = Some((PieceKind::Knight, Color::Black));
     board[7][7] = Some((PieceKind::Rook, Color::Black));
     for n in 0..8 {
-        board[6][n] = Some((PieceKind::Pawn, Color::Black));
+        board[n][6] = Some((PieceKind::Pawn, Color::Black));
     }
 }
 
@@ -83,8 +83,6 @@ fn activate_selected_color(i: u8, j: u8, selected: bool) -> String {
 #[inline_props]
 #[allow(non_snake_case)]
 fn Piece(cx: Scope<()>, board: UseRef<ChessBoard>, position: (u8, u8)) -> Element {
-    let piece = use_state(cx, || String::new());
-    info!("Rendering Piece...");
     if board.read().board[position.0 as usize][position.1 as usize].is_none() {
         return None;
     }
@@ -92,31 +90,22 @@ fn Piece(cx: Scope<()>, board: UseRef<ChessBoard>, position: (u8, u8)) -> Elemen
     let (kind, color) = binding.board[position.0 as usize][position.1 as usize]
         .as_ref()
         .unwrap();
-    piece.set(
-        (match kind {
-            PieceKind::Pawn => "pawn_",
-            PieceKind::Knight => "knight_",
-            PieceKind::Bishop => "bishop_",
-            PieceKind::Rook => "rook_",
-            PieceKind::Queen => "queen_",
-            PieceKind::King => "king_",
-        })
-        .to_string()
-            + match color {
-                Color::White => "white",
-                Color::Black => "black",
-            },
-    );
-    let src = format!("assets/{}.png", piece.get());
-    info!("Rendering Piece...");
-    cx.render(rsx! {
-        img {
-            src: "{src}",
-            onclick: move |_| {
-                info!("Clicked on piece {:?}", piece);
-            }
-        }
+    let piece = (match kind {
+        PieceKind::Pawn => "pawn_",
+        PieceKind::Knight => "knight_",
+        PieceKind::Bishop => "bishop_",
+        PieceKind::Rook => "rook_",
+        PieceKind::Queen => "queen_",
+        PieceKind::King => "king_",
     })
+    .to_string()
+        + match color {
+            Color::White => "white",
+            Color::Black => "black",
+        };
+
+    let src = use_state(cx, || format!("{}.png", piece));
+    cx.render(rsx! { img { src: "{src}", class: "piece" } })
 }
 
 #[derive(PartialEq, Props)]
